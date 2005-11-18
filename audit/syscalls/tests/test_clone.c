9eb8472309e9891bf234879a1712751f60331e94
amg <amg@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2005-11-18 17:20:58 +0000

    Fix tests for clone, clone2.
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@565 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

--- audit/syscalls/tests/test_clone.c
+++ audit/syscalls/tests/test_clone.c
@@ -1,136 +1,106 @@
-/**********************************************************************
- **   Copyright (C) International Business Machines  Corp., 2003
- **
- **   This program is free software;  you can redistribute it and/or modify
- **   it under the terms of the GNU General Public License as published by
- **   the Free Software Foundation; either version 2 of the License, or
- **   (at your option) any later version.
- **
- **   This program is distributed in the hope that it will be useful,
- **   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- **   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- **   the GNU General Public License for more details.
- **
- **   You should have received a copy of the GNU General Public License
- **   along with this program;  if not, write to the Free Software
- **   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- **
- **
- **
- **  FILE       : test_clone.c
- **
- **  PURPOSE    : To test the clone library call auditing.
- **
- **  DESCRIPTION: The test_clone() function builds into the
- **  laus_test framework to verify that the Linux Audit System
- **  accurately logs both successful and erroneous execution of the
- **  "clone" system call.
- **
- **  In the successful case, this function:
- **   1) Malloc some memory for the child process stack
- **   2) Preform the syscall(__NR_clone ...  with the VFORK flag so 
- **      the parent waits.
- **   3) Clean up by freeing the child stack.
- **
- **  The successful case does a syscall(__NR_clone ...in the VFORK 
- **  mode.  The parent will wait and then finish the post_syscall 
- **  and clean-up stuff.
- **
- **  In the erroneous case, this function:
- **   1) Preforms the syscall(__NR_clone ... with the VFORK flag and 
- **      the CLONE_PID a NULL value for the stack.  This will only 
- **      fail if user is NOT root.
- **
- **  The erroneous case causes a failure by passing the CLONE_NEWNS flag.
- **  The error is EPERM(1).
- **
- **  HISTORY    :
- **    06/03 Originated by Kylene J. Smith <kylene@us.ibm.com>
- **    03/04 Added exp_errno variable by D. Kent Soper <dksoper@us.ibm.com>
- **    04/04 Modified test to call glibc interface by Kimberly D. Simon <kdsimon@us.ibm.com>
- **
- **********************************************************************/
+/*  Copyright (C) International Business Machines  Corp., 2003
+ *  (c) Copyright Hewlett-Packard Development Company, L.P., 2005
+ *
+ *  This program is free software;  you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *  the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program;  if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ *  Implementation written by HP, based on original code from IBM.
+ *
+ *  FILE:
+ *  test_clone.c
+ *
+ *  PURPOSE:
+ *  Verify audit of attempts to create child processes.
+ *
+ *  SYSCALLS:
+ *  clone(), clone2
+ *
+ *  TESTCASE: successful
+ *  Create a child process with CLONE_NEWNS as root user.
+ *
+ *  TESTCASE: unsuccessful
+ *  Attempt to create a child process with CLONE_NEWNS as test user.
+ */
 
 #include "includes.h"
 #include "syscalls.h"
 #include <sched.h>
 
-#ifndef __ia64__
-int fn(void *x)
+#define CHILD_STACK_MEM 65536
+
+static int common_clone(struct audit_data *context)
 {
-    sleep(1);
-    return 0;
+    int rc = 0;
+    int success = context->success; /* save intended result */
+    int flags = CLONE_NEWNS|CLONE_VFORK; /* wait for child exit */
+    char *cstack;
+    pid_t pid;
+
+    /* Allocate memory for child stack */
+    errno = 0;
+    cstack = malloc(CHILD_STACK_MEM); 
+    if (!cstack) {
+	fprintf(stderr, "Error: malloc(): %s\n", strerror(errno));
+	goto exit;
+    }
+    fprintf(stderr, "Allocated %d for child stack\n", CHILD_STACK_MEM);
+
+    /* To produce failure, attempt to use CLONE_NEWNS as unprivileged user */
+    if (!success) {
+	rc = seteuid_test();
+	if (rc < 0)
+	    goto exit_free;
+	context->experror = EPERM;
+    }
+
+    rc = context_setidentifiers(context);
+    if (rc < 0)
+	goto exit_free;
+
+    errno = 0;
+    context_setbegin(context);
+    fprintf(stderr, "Create child at %p\n", cstack);
+    pid = syscall(context->u.syscall.sysnum, flags, cstack);
+
+    if (pid == 0) /* child */
+	_exit(0);
+
+    context_setend(context);
+
+    if (pid < 0) {
+	context->success = 0;
+	context->u.syscall.exit = context->error = errno;
+    } else {
+	context->success = 1;
+	context->u.syscall.exit = pid;
+    }
+
+exit_free:
+    free(cstack);
+
+exit:
+    if (!success)
+	seteuid(0); /* clean up from failure case */
+    return rc;
 }
-#endif
 
 int test_clone(struct audit_data *context)
 {
-    int rc = 0;
-    int exp_errno = EPERM;
-    int flags = CLONE_VFORK;
-    // int flags = CLONE_FS|CLONE_VFORK|CLONE_PARENT|CLONE_SYSVSEM|0x8000068;
-    pid_t pid;
-    char *stack = NULL;
+    return common_clone(context);
+}
 
-    // Do as much setup work as possible right here
-    if (context->success) {	// Set up for success
-
-    } else {			// Set up for error
-
-	// BUGBUG: For some reason, we are not able to fail the the clone() syscall
-	//   in the 2.6 kernel.
-//              flags |= CLONE_NEWNS;
-	rc = SKIP_TEST_CASE;
-	goto EXIT;
-
-    }
-
-    // Set up audit argument buffer
-    // Only save off first argument to assist log verification
-    if ((rc =
-	 auditArg1(context, AUDIT_ARG_IMMEDIATE, sizeof(int), &flags)) != 0) {
-	fprintf(stderr, "Error setting up audit argument buffer\n");
-	goto EXIT_CLEANUP;
-    }
-    // Do pre-system call work
-    if ((rc = preSysCall(context)) != 0) {
-	fprintf(stderr, "ERROR: pre-syscall setup failed (%d)\n", rc);
-	goto EXIT_CLEANUP;
-    }
-    // Execute system call--parent waits b/c of CLONE_VFORK flag
-#ifdef __ia64__
-    // ia64 glibc doesn't have a symbol for clone
-    pid = syscall(__NR_clone, flags, NULL);
-#else
-    stack = malloc(65536);
-    pid = clone(fn, (void *)(stack + 32768), flags, NULL);
-#endif
-    switch (pid) {
-	case -1:
-	    fprintf(stderr, "ERROR: clone failed (%d)\n", pid);
-	    goto EXIT_CLEANUP;
-	case 0:
-	    //In child
-	    _exit(0);
-	default:
-	    //In parent
-	    context->u.syscall.exit = pid;
-    }
-
-    // Do post-system call work
-    if ((rc = postSysCall(context, errno, -1, exp_errno)) != 0) {
-	fprintf(stderr, "ERROR: post-syscall setup failed (%d)\n", rc);
-	goto EXIT_CLEANUP;
-    }
-
-
-EXIT_CLEANUP:
-
-EXIT:
-    if (stack) {
-	free(stack);
-    }
-
-    fprintf(stderr, "Returning from test\n");
-    return rc;
+int test_clone2(struct audit_data *context)
+{
+    return common_clone(context);
 }

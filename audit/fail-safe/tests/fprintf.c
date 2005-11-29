1b75b019b162c8970e40dfe6842fa95e90657e0e
agriffis <agriffis@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2005-11-29 20:52:53 +0000

    Remove unused dlopen code
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@724 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

--- audit/fail-safe/tests/fprintf.c
+++ audit/fail-safe/tests/fprintf.c
@@ -1,31 +1,8 @@
-#include <dlfcn.h>
 #include <errno.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-
-#if 0
-static void *libc_handle = NULL;
-int (*true_fprintf)(FILE *stream, const char *format, ...);
-
-void __attribute__ ((constructor)) libfprintf_init(void)
-{
-    printf("Initializing libfprintf\n");
-
-    libc_handle = dlopen("libc.so.6.1", RTLD_LAZY);
-    if (!libc_handle) {
-        printf("libfprintf: can't dlopen libc: %s\n", dlerror());
-        exit(9);
-    }
-
-    true_fprintf = dlsym(libc_handle, "fprintf");
-    if (!true_fprintf) {
-        printf("libfprintf: can't resolve fprintf: %s\n", dlerror());
-        exit(9);
-    }
-}
-#endif
 
 int fprintf(FILE *stream, const char *format, ...)
 {

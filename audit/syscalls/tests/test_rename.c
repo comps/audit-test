b4e684cf492ce32376e0c1cd635c1f33ff5d9b4e
amg <amg@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2005-12-01 22:13:03 +0000

    Complete syscall test for rename.
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@786 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

--- audit/syscalls/tests/test_rename.c
+++ audit/syscalls/tests/test_rename.c
@@ -1,51 +1,38 @@
-/**********************************************************************
-   **   Copyright (C) International Business Machines  Corp., 2003
-   **
-   **   This program is free software;  you can redistribute it and/or modify
-   **   it under the terms of the GNU General Public License as published by
-   **   the Free Software Foundation; either version 2 of the License, or
-   **   (at your option) any later version.
-   **
-   **   This program is distributed in the hope that it will be useful,
-   **   but WITHOUT ANY WARRANTY;  without even the implied warranty of
-   **   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
-   **   the GNU General Public License for more details.
-   **
-   **   You should have received a copy of the GNU General Public License
-   **   along with this program;  if not, write to the Free Software
-   **   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-   **
-   **
-   **
-   **  FILE       : test_rename.c
-   **
-   **  PURPOSE    : To test the rename library call auditing.
-   **
-   **  DESCRIPTION: The test_rename() function builds into the
-   **  laus_test framework to verify that the Linux Audit System
-   **  accurately logs both successful and erroneous execution of the
-   **  "rename" system call.
-   **
-   **  In the successful case, this function:
-   **   1) Generates an originating temporary file
-   **   2) Generates a target temporary filename
-   **   3) Makes the rename syscall 
-   **
-   **  The successful case renames the originating file to
-   **  the target filename.  Since the originating filename
-   **  is created immediately before renaming it to another unique,
-   **  nonexistent name, we can expect rename() to successfully execute.
-   **  
-   **  The erroneous case invokes an error by trying to rename a file
-   **  into the /root directory as a non-root user.
-   **  According to the man page, an EACCES error is generated.
-   **
-   **  HISTORY    :
-   **    06/03 Originated by Michael A. Halcrow <mike@halcrow.us>
-   **    06/03 Furthered by Dustin Kirkland <k1rkland@us.ibm.com>
-   **    03/04 Added exp_errno variable by D. Kent Soper <dksoper@us.ibm.com>
-   **
-   **********************************************************************/
+/*  Copyright (C) International Business Machines  Corp., 2003
+ *  (c) Copyright Hewlett-Packard Development Company, L.P., 2005
+ *
+ *  This program is free software;  you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *  the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program;  if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ *  Implementation written by HP, based on original code from IBM.
+ *
+ *  FILE:
+ *  test_rename.c
+ *
+ *  PURPOSE:
+ *  Verify audit of attempts change the name or location of a file.
+ *
+ *  SYSCALLS:
+ *  rename()
+ *
+ *  TESTCASE: successful
+ *  Rename a file to the name of an existing file.
+ *
+ *  TESTCASE: unsuccessful
+ *  Attempt to rename a file to the name of an existing file for which
+ *  user does not have appropriate permissions to modify.
+ */
 
 #include "includes.h"
 #include "syscalls.h"
@@ -53,87 +40,52 @@
 
 int test_rename(struct audit_data *context, int variation, int success)
 {
-
     int rc = 0;
-    int exp_errno = -EACCES;
+    char *oldpath, *newpath;
+    int exit = -1;
 
-    char *path = NULL;
-    char *targetPath = NULL;
-    char *res = NULL;
-
-    path = init_tempfile(S_IRWXU|S_IRWXG|S_IRWXO, context->euid,
-			 context->egid);
-    if (!path) {
+    oldpath = init_tempfile(S_IRWXU|S_IRWXO, context->euid, context->egid);
+    newpath = init_tempfile(S_IRWXU, context->euid, context->egid);
+    if (!oldpath || !newpath) {
 	rc = -1;
-	goto EXIT;
+	goto exit;
     }
 
-    if (context->success) {
-	targetPath = init_tempfile(S_IRWXU, context->euid, context->egid); 
-	if (!targetPath || (unlink(targetPath) < 0)) {
-	    fprintf(stderr, "ERROR: Cannot reserve filename %s\n", targetPath);
-	    goto EXIT_CLEANUP;
-	}
-    } else {
-	targetPath = strdup("/root/");
-	res = realloc(targetPath, strlen(path));
-	if (!res) {
-	    fprintf(stderr, "ERROR: Unable to realloc memory\n");
-	    goto EXIT_CLEANUP;
-	}
-	strcat(targetPath, basename(path));
+    if (!success) {
+	rc = seteuid_test();
+	if (rc < 0)
+	    goto exit_path;
+	context->experror = -EPERM;
     }
 
-    // Set up audit argument buffer
-    if ((rc = auditArg2(context, AUDIT_ARG_PATH, strlen(path), path,
-			context->success ? AUDIT_ARG_PATH : AUDIT_ARG_STRING,
-			strlen(targetPath), targetPath)) != 0) {
-	fprintf(stderr, "Error setting up audit argument buffer\n");
-	goto EXIT_CLEANUP;
-    }
-    // Fill in struct audit_data structure
-    fprintf(stderr, "Calling getLAUSData\n");
-    if ((rc = getLAUSData(context)) != 0) {
-	fprintf(stderr, "Error returned from getLAUSData( context ): rc=%i\n", rc);
-	goto EXIT_CLEANUP;
-    }
-    // Do pre-system call work
-    if ((rc = preSysCall(context)) != 0) {
-	fprintf(stderr, "ERROR: pre-syscall setup failed (%d)\n", rc);
-	goto EXIT_CLEANUP;
-    }
-    // Execute system call
-    context->u.syscall.exit = syscall(__NR_rename, path, targetPath);
+    rc = context_setcwd(context);
+    if (rc < 0)
+	goto exit_suid;
+    context_setsobj(context, oldpath);
+    context_settobj(context, newpath);
 
-    // Do post-system call work
-    if ((rc = postSysCall(context, errno, -1, exp_errno)) != 0) {
-	fprintf(stderr, "ERROR: post-syscall setup failed (%d)\n", rc);
-	goto EXIT_CLEANUP;
-    }
+    rc = context_setidentifiers(context);
+    if (rc < 0)
+	goto exit_suid;
 
-EXIT_CLEANUP:
-    if (context->success) {
-	// remove the target temporary file
-	fprintf(stderr, "Removing file %s\n", targetPath);
-	if (unlink(targetPath) == -1) {
-	    fprintf(stderr, "Error removing file %s: errno=%i\n", targetPath, errno);
-	    goto EXIT;
-	}
-    } else {
-	fprintf(stderr, "Removing file %s\n", path);
-	if (unlink(path) == -1) {
-	    fprintf(stderr, "Error removing file %s: errno=%i\n", path, errno);
-	    goto EXIT;
-	}
-    }
+    errno = 0;
+    context_setbegin(context);
+    fprintf(stderr, "Attempting rename(%s, %s)\n", oldpath, newpath);
+    exit = syscall(context->u.syscall.sysnum, oldpath, newpath);
+    context_setend(context);
+    context_setresult(context, exit, errno);
 
-EXIT:
-    if (targetPath) {
-	free(targetPath);
-    }
-    if (path) {
-	free(path);
-    }
-    fprintf(stderr, "Returning from test\n");
+exit_suid:
+    if (!success && seteuid(0) < 0)
+	fprintf(stderr, "Error: seteuid(0): %s\n", strerror(errno));
+
+exit_path:
+    if (exit < 0)
+	destroy_tempfile(oldpath);
+    else
+	free(oldpath);
+    destroy_tempfile(newpath);
+
+exit:
     return rc;
 }

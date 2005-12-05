8fe633421fa9ab2aa0bbdf159e9f9fec550261d7
amg <amg@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2005-12-05 20:58:46 +0000

    Complete tests for link, symlink.
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@832 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

--- audit/syscalls/tests/test_link.c
+++ audit/syscalls/tests/test_link.c
@@ -1,57 +1,39 @@
-/**********************************************************************
-   **   Copyright (C) International Business Machines  Corp., 2003
-   **
-   **   This program is free software;  you can redistribute it and/or modify
-   **   it under the terms of the GNU General Public License as published by
-   **   the Free Software Foundation; either version 2 of the License, or
-   **   (at your option) any later version.
-   **
-   **   This program is distributed in the hope that it will be useful,
-   **   but WITHOUT ANY WARRANTY;  without even the implied warranty of
-   **   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
-   **   the GNU General Public License for more details.
-   **
-   **   You should have received a copy of the GNU General Public License
-   **   along with this program;  if not, write to the Free Software
-   **   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-   **
-   **
-   **
-   **  FILE       : test_link.c
-   **
-   **  PURPOSE    : To test the link library call auditing.
-   **
-   **  DESCRIPTION: The test_link_eacces() function builds into the
-   **  laus_test framework to verify that the Linux Audit System
-   **  accurately logs both successful and erroneous execution of the
-   **  "link" system call.
-   **
-   **  In the successful case, this function:
-   **   1) Generates a unique filename and creates a temporary file
-   **   2) Generates a unique filename to use as the link name
-   **   3) Clears the audit trail
-   **   4) Executes the "link" system call
-   **   5) Tests the results of the system call against the
-   **      expected successful return
-   **
-   **  The successful case executes the expected conditions described
-   **  by the "link" system call manpage.  That is, the link() function
-   **  is called with an existing source file and a valid destination
-   **  name.
-   **  
-   **  In the erroneous case, this function:
-   **    1) Tries to create a link in /root as a non-root user
-   **      
-   **  The erroneous case executes the expected conditions described by 
-   **  the "link" system call manpage for erroneous input.  The link()
-   **  function is called as a non-root user to put a link in the /root
-   **  directory.  This should throw an EACCES errno.
-   **
-   **  HISTORY    :
-   **    06/03 Originated by Dustin Kirkland (k1rkland@us.ibm.com)
-   **    03/04 Added exp_errno variable by D. Kent Soper <dksoper@us.ibm.com>
-   **
-   **********************************************************************/
+/*  Copyright (C) International Business Machines  Corp., 2003
+ *  (c) Copyright Hewlett-Packard Development Company, L.P., 2005
+ *
+ *  This program is free software;  you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *  the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program;  if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ *  Implementation written by HP, based on original code from IBM.
+ *
+ *  FILE:
+ *  test_link.c
+ *
+ *  PURPOSE:
+ *  Verify audit of attempts to create hard links.
+ *
+ *  SYSCALLS:
+ *  link()
+ *
+ *  TESTCASE: successful
+ *  Create a hard link in a directory for which user has appropriate
+ *  permissions.
+ *
+ *  TESTCASE: unsuccessful
+ *  Attempt to create a hard link in a directory for which user does
+ *  not have appropriate permissions.
+ */
 
 #include "includes.h"
 #include "syscalls.h"
@@ -59,90 +41,80 @@
 
 int test_link(struct audit_data *context, int variation, int success)
 {
-
     int rc = 0;
-    int exp_errno = -EACCES;
+    char *oldpath, *newpath;
+    char *lname = "/link";
+    int exit;
 
-    char *source = NULL;
-    char *destination = NULL;
-    char *res = NULL;
-
-    source = init_tempfile(S_IRWXU|S_IRWXG|S_IRWXO, context->euid,
-			   context->egid);
-    if (!source) {
+    oldpath = init_tempfile(S_IRWXU|S_IRWXO, context->euid, context->egid);
+    if (!oldpath) {
 	rc = -1;
-	goto EXIT;
+	goto exit;
     }
 
-    if (context->success) {
-	// In success case, create file name to guarantee uniqueness, then delete it
-	destination = init_tempfile(S_IRWXU|S_IRWXG|S_IRWXO,
-				    context->euid, context->egid);
-	if (!destination) {
-	    rc = -1;
-	    goto EXIT;
-	}
-	if ((rc = unlink(destination)) != 0) {
-	    fprintf(stderr, "ERROR: Unable to remove file %s: errno=%i\n",
-		    destination, errno);
-	    goto EXIT;
-	}
-    } else {
-	// Fail case, so try to create link in /root as non-root user
-
-	/* ignore leading directories on tempfile path */
-	destination = strdup("/root/");
-	res = realloc(destination, strlen(source));
-	if (!res) {
-	    fprintf(stderr, "ERROR: Unable to realloc memory\n");
-	    goto EXIT_CLEANUP;
-	}
-	strcat(destination, basename(source));
-	context->euid = context->fsuid = helper_uid;
+    newpath = init_tempdir(S_IRWXU, context->euid, context->egid);
+    if (!newpath) {
+	destroy_tempfile(oldpath);
+	rc = -1;
+	goto exit;
     }
 
-    // Set up audit argument buffer
-    if ((rc = auditArg2(context, AUDIT_ARG_PATH, strlen(source), source,
-			context->success ? AUDIT_ARG_PATH : AUDIT_ARG_STRING,
-			strlen(destination), destination)) != 0) {
-	fprintf(stderr, "Error setting up audit argument buffer\n");
-	goto EXIT;
-    }
-    // Do pre-system call work
-    if ((rc = preSysCall(context)) != 0) {
-	fprintf(stderr, "ERROR: pre-syscall setup failed (%d)\n", rc);
-	goto EXIT_CLEANUP;
-    }
-    // Execute system call
-    context->u.syscall.exit = syscall(__NR_link, source, destination);
-
-    // Do post-system call work
-    if ((rc = postSysCall(context, errno, -1, exp_errno)) != 0) {
-	fprintf(stderr, "ERROR: post-syscall setup failed (%d)\n", rc);
-	goto EXIT_CLEANUP;
+    errno = 0;
+    if (realloc(newpath, strlen(newpath) + sizeof(lname)) == NULL) {
+	fprintf(stderr, "Error: initializing path: realloc(): %s\n",
+		strerror(errno));
+	rc = -1;
+	goto exit_path;
     }
 
-EXIT_CLEANUP:
-    if ((unlink(source)) != 0) {
-	fprintf(stderr, "ERROR: Unable to remove file %s: errno=%i\n", source, errno);
-	goto EXIT;
-    }
-    if (context->success) {
-	if ((unlink(destination)) != 0) {
-	    fprintf(stderr, "ERROR: Unable to remove file %s: errno=%i\n",
-		    destination, errno);
-	    goto EXIT;
-	}
+    errno = 0;
+    if (strcat(newpath, lname) == NULL) {
+	fprintf(stderr, "Error: initializing path: strcat(): %s\n",
+		strerror(errno));
+	rc = -1;
+    goto exit_path;
     }
 
-EXIT:
-    if (source) {
-	free(source);
+    if (!success) {
+	rc = seteuid_test();
+	if (rc < 0)
+	    goto exit_path;
+	context->experror = -EACCES;
     }
-    if (destination) {
-	free(destination);
-    }
-    fprintf(stderr, "Returning from test\n");
 
+    rc = context_setcwd(context);
+    if (rc < 0)
+	goto exit_suid;
+    /* We are setting the source and target objects opposite of what
+     * would be expected, to match the ordering in the audit record.
+     * The audit record doesn't actually call them "source" or
+     * "target" as this testsuite does, so it wouldn't be considered a
+     * product bug. */
+    context_settobj(context, oldpath);
+    context_setsobj(context, newpath);
+
+    rc = context_setidentifiers(context);
+    if (rc < 0)
+	goto exit_suid;
+
+    errno = 0;
+    context_setbegin(context);
+    fprintf(stderr, "Attempting link(%s, %s)\n", oldpath, newpath);
+    exit = syscall(context->u.syscall.sysnum, oldpath, newpath);
+    context_setend(context);
+    context_setresult(context, exit, errno);
+
+    if ((exit == 0) && (unlink(newpath) < 0))
+	fprintf(stderr, "Error: removing file: %s\n", strerror(errno));
+
+exit_suid:
+    if (!success && seteuid(0) < 0)
+	fprintf(stderr, "Error: seteuid(0): %s\n", strerror(errno));
+
+exit_path:
+    destroy_tempfile(oldpath);
+    destroy_tempdir(dirname(newpath));
+
+exit:
     return rc;
 }
--- audit/syscalls/tests/test_symlink.c
+++ audit/syscalls/tests/test_symlink.c
@@ -1,55 +1,39 @@
-/**********************************************************************
- **   Copyright (C) International Business Machines  Corp., 2003
- **
- **   This program is free software;  you can redistribute it and/or modify
- **   it under the terms of the GNU General Public License as published by
- **   the Free Software Foundation; either version 2 of the License, or
- **   (at your option) any later version.
- **
- **   This program is distributed in the hope that it will be useful,
- **   but WITHOUT ANY WARRANTY;  without even the implied warranty of
- **   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
- **   the GNU General Public License for more details.
- **
- **   You should have received a copy of the GNU General Public License
- **   along with this program;  if not, write to the Free Software
- **   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- **
- **
- **
- **  FILE       : test_symlink.c
- **
- **  PURPOSE    : To test the symlink library call auditing.
- **
- **  DESCRIPTION: The test_symlink() function builds into the
- **  laus_test framework to verify that the Linux Audit System
- **  accurately logs both successful and erroneous execution of the
- **  "symlink" system call.
- **
- **  In the successful case, this function:
- **   1) Generates a unique filename and creates a temporary file
- **   2) Generates a unique filename to use as the symlink name
- **   3) Executes the "symlink" system call
- **
- **  The successful case executes the expected conditions described
- **  by the "symlink" system call manpage.  That is, the symlink() function
- **  is called with an existing source file and a valid destination
- **  name.
- **  
- **  In the erroneous case, this function:
- **   1) Execute the "symlink" system call with two paths, the destination 
- **      which the user doesn't have access to
- **      
- **  The erroneous case executes the expected conditions described by 
- **  the "symlink" system call manpage for the EACCES errno.  The symlink()
- **  function is called with unaccessible destination path.
- **
- **  HISTORY    :
- **    06/03 Originated by Dustin Kirkland (k1rkland@us.ibm.com)
- **    10/03 Furthered by Kylene J. Smith (kylene@us.ibm.com)
- **    03/04 Added exp_errno variable by D. Kent Soper <dksoper@us.ibm.com>
- **
- **********************************************************************/
+/*  Copyright (C) International Business Machines  Corp., 2003
+ *  (c) Copyright Hewlett-Packard Development Company, L.P., 2005
+ *
+ *  This program is free software;  you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY;  without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+ *  the GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program;  if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ *  Implementation written by HP, based on original code from IBM.
+ *
+ *  FILE:
+ *  test_symlink.c
+ *
+ *  PURPOSE:
+ *  Verify audit of attempts to create symbolic links.
+ *
+ *  SYSCALLS:
+ *  symlink()
+ *
+ *  TESTCASE: successful
+ *  Create a symlink in a directory for which user has appropriate
+ *  permissions.
+ *
+ *  TESTCASE: unsuccessful
+ *  Attempt to create a symlink in a directory for which user does
+ *  not have appropriate permissions.
+ */
 
 #include "includes.h"
 #include "syscalls.h"
@@ -58,80 +42,74 @@
 int test_symlink(struct audit_data *context, int variation, int success)
 {
     int rc = 0;
-    int exp_errno = -EACCES;
+    char *oldpath, *newpath;
+    char *lname = "/link";
+    int exit = -1;
 
-    char *source = NULL;
-    char *destination = NULL;
-    char *res = NULL;
-
-    source = init_tempfile(S_IRWXU|S_IRWXG|S_IRWXO, context->euid,
-			   context->egid);
-    if (!source) {
+    oldpath = init_tempfile(S_IRWXU|S_IRWXO, context->euid, context->egid);
+    if (!oldpath) {
 	rc = -1;
-	goto EXIT;
+	goto exit;
     }
 
-    if (context->success) {
-	destination = init_tempfile(S_IRWXU, context->euid, context->egid);
-	if (!destination || (unlink(destination) < 0)) {
-	    fprintf(stderr, "ERROR: Cannot reserve filename %s\n", destination);
-	    goto EXIT_CLEANUP;
-	}
-    } else {
-	// Fail case, so try to create link in /root as non-root user
-
-	/* ignore leading directories on tempfile path */
-	destination = strdup("/root/");
-	res = realloc(destination, strlen(source));
-	if (!res) {
-	    fprintf(stderr, "ERROR: Unable to realloc memory\n");
-	    goto EXIT_CLEANUP;
-	}
-	strcat(destination, basename(source));
-	context->euid = context->fsuid = helper_uid;
+    newpath = init_tempdir(S_IRWXU, context->euid, context->egid);
+    if (!newpath) {
+	destroy_tempfile(oldpath);
+	rc = -1;
+	goto exit;
     }
 
-    // Set up audit argument buffer
-    if ((rc = auditArg2(context, AUDIT_ARG_STRING, strlen(source), source,
-			context->success ? AUDIT_ARG_PATH : AUDIT_ARG_STRING,
-			strlen(destination), destination)) != 0) {
-	fprintf(stderr, "Error setting up audit argument buffer\n");
-	goto EXIT_CLEANUP;
-    }
-    // Do pre-system call work
-    if ((rc = preSysCall(context)) != 0) {
-	fprintf(stderr, "ERROR: pre-syscall setup failed (%d)\n", rc);
-	goto EXIT_CLEANUP;
-    }
-    // Execute system call
-    context->u.syscall.exit = syscall(__NR_symlink, source, destination);
-
-    // Do post-system call work
-    if ((rc = postSysCall(context, errno, -1, exp_errno)) != 0) {
-	fprintf(stderr, "ERROR: post-syscall setup failed (%d)\n", rc);
-	goto EXIT_CLEANUP;
+    errno = 0;
+    if (realloc(newpath, strlen(newpath) + sizeof(lname)) == NULL) {
+	fprintf(stderr, "Error: initializing path: realloc(): %s\n",
+		strerror(errno));
+	rc = -1;
+	goto exit_path;
     }
 
+    errno = 0;
+    if (strcat(newpath, lname) == NULL) {
+	fprintf(stderr, "Error: initializing path: strcat(): %s\n",
+		strerror(errno));
+	rc = -1;
+    goto exit_path;
+    }
 
-EXIT_CLEANUP:
-    if ((unlink(source)) != 0) {
-	fprintf(stderr, "ERROR: Unable to remove file %s: errno=%i\n", source, errno);
-	goto EXIT;
+    if (!success) {
+	rc = seteuid_test();
+	if (rc < 0)
+	    goto exit_path;
+	context->experror = -EACCES;
     }
-    if (context->success) {
-	if ((unlink(destination)) != 0) {
-	    fprintf(stderr, "ERROR: Unable to remove file %s: errno=%i\n",
-		    destination, errno);
-	    goto EXIT;
-	}
-    }
-EXIT:
-    if (source) {
-	free(source);
-    }
-    if (destination) {
-	free(destination);
-    }
-    fprintf(stderr, "Returning from test\n");
+
+    rc = context_setcwd(context);
+    if (rc < 0)
+	goto exit_suid;
+    context_settobj(context, newpath);
+    context_setsobj(context, oldpath);
+
+    rc = context_setidentifiers(context);
+    if (rc < 0)
+	goto exit_suid;
+
+    errno = 0;
+    context_setbegin(context);
+    fprintf(stderr, "Attempting symlink(%s, %s)\n", oldpath, newpath);
+    exit = syscall(context->u.syscall.sysnum, oldpath, newpath);
+    context_setend(context);
+    context_setresult(context, exit, errno);
+
+    if ((exit == 0) && (unlink(newpath) < 0))
+	fprintf(stderr, "Error: removing file: %s\n", strerror(errno));
+
+exit_suid:
+    if (!success && seteuid(0) < 0)
+	fprintf(stderr, "Error: seteuid(0): %s\n", strerror(errno));
+
+exit_path:
+    destroy_tempfile(oldpath);
+    destroy_tempdir(dirname(newpath));
+
+exit:
     return rc;
 }

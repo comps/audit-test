15b26453f26ef0c104ace75df7dd6467f54695e0
pmoore <pmoore@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2005-12-06 21:25:12 +0000

    The syscall attribute test, this test will currently fail due to RH bugzilla #175132
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@858 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

--- audit/filter/tests/syscall_attrs.sh
+++ audit/filter/tests/syscall_attrs.sh
@@ -51,11 +51,11 @@ source functions.bash
 # override the test harness cleanup function
 prepend_cleanup '
     # remove the filter we set earlier
-    [ -n "$filter_field" ] && auditctl -d $filter_rule $filter_field 2>/dev/null'
+    auditctl -d $filter_rule $filter_field 2>/dev/null'
 
-# generate an audit record for the given file and update the
-# audit log marker
-function audit_rec_gen {
+# generate a successful audit record for the given file and update the audit
+# log marker
+function audit_rec_gen_ok {
     if [ -f "$1" ]; then
         log_mark=$(stat -c %s $audit_log)
         cat "$1" > /dev/null
@@ -63,6 +63,18 @@ function audit_rec_gen {
         exit_error "unable to find file \"$1\""
     fi
 }
+
+# generate a failure audit record for the given file and update the audit log
+# marker
+function audit_rec_gen_fail {
+    if [ -f "$1" ]; then
+        log_mark=$(stat -c %s $audit_log)
+        /bin/su $TEST_USER bash -c "cat \"$1\"" 2> /dev/null
+    else
+        exit_error "unable to find file \"$1\""
+    fi
+}
+
 
 #
 # main
@@ -81,6 +93,8 @@ log_mark=$(stat -c %s $audit_log)
 # create the test files
 echo "notice: creating the test file ..."
 touch $tmp1 2> /dev/null || exit_error "unable to create temporary file for testing"
+chmod 0600 $tmp1 || exit_error "unable to set the permissions on the test file"
+chown root:root $tmp1 || exit_error "unable to set the permissions on the test file"
 
 # get syscall information
 syscall_name="open"
@@ -102,24 +116,109 @@ filter_field="-F success=1"
 auditctl -a $filter_rule $filter_field
 
 # generate an audit event
-audit_rec_gen $tmp1
+audit_rec_gen_ok $tmp1
 
 # check for the audit record
 echo "notice: testing for audit record ..."
 augrep --seek=$log_mark "syscall==$syscall_num" "name==$tmp1" "success==yes"
-ret_val=$?
+ret_val_tmp=$?
+[ "$ret_val" = "0" ] && ret_val=$ret_val_tmp
+
+# display the result
+if [ "$ret_val_tmp" = "0" ]; then
+    echo "notice: found audit record - PASS"
+else
+    echo "notice: did not find audit record - FAIL"
+fi
+
+# remove the filter
+auditctl -d $filter_rule $filter_field
+filter_field=""
 
 ### fail check
 
 echo ""
 
+# set an audit filter
+echo "notice: setting a filter for the syscall failure ..."
+filter_field="-F success=0"
+auditctl -a $filter_rule $filter_field
+
+# generate an audit event
+audit_rec_gen_fail $tmp1
+
+# check for the audit record
+echo "notice: testing for audit record ..."
+augrep --seek=$log_mark "name==$tmp1"
+ret_val_tmp=$?
+[ "$ret_val" = "0" ] && ret_val=$ret_val_tmp
+
+# display the result
+if [ "$ret_val_tmp" = "0" ]; then
+    echo "notice: found audit record - PASS"
+else
+    echo "notice: did not find audit record - FAIL"
+fi
+
+# remove the filter
+auditctl -d $filter_rule $filter_field
+filter_field=""
+
 ### syscall name check
 
 echo ""
 
+# set an audit filter
+echo "notice: setting a filter for the syscall name ..."
+filter_rule="exit,always -S open"
+auditctl -a $filter_rule
+
+# generate an audit event
+audit_rec_gen_ok $tmp1
+
+# check for the audit record
+echo "notice: testing for audit record ..."
+augrep --seek=$log_mark "name==$tmp1" "syscall==$syscall_num"
+ret_val_tmp=$?
+[ "$ret_val" = "0" ] && ret_val=$ret_val_tmp
+
+# display the result
+if [ "$ret_val_tmp" = "0" ]; then
+    echo "notice: found audit record - PASS"
+else
+    echo "notice: did not find audit record - FAIL"
+fi
+
+# remove the filter
+auditctl -d $filter_rule
+
 ### syscall number check
 
 echo ""
+
+# set an audit filter
+echo "notice: setting a filter for the syscall number ..."
+filter_rule="exit,always -S $syscall_num"
+auditctl -a $filter_rule
+
+# generate an audit event
+audit_rec_gen_ok $tmp1
+
+# check for the audit record
+echo "notice: testing for audit record ..."
+augrep --seek=$log_mark "name==$tmp1" "syscall==$syscall_num"
+ret_val_tmp=$?
+[ "$ret_val" = "0" ] && ret_val=$ret_val_tmp
+
+# display the result
+if [ "$ret_val_tmp" = "0" ]; then
+    echo "notice: found audit record - PASS"
+else
+    echo "notice: did not find audit record - FAIL"
+fi
+
+# remove the filter
+auditctl -d $filter_rule
 
 #
 # done

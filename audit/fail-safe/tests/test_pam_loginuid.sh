253ca3b26e0d94e247c6388a442c538dec01846b
amg <amg@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2006-11-30 19:06:11 +0000

    - make a pass/fail output line item for each pam_loginuid test - the expect call doesn't need to be in a separate file
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@1226 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

--- audit/fail-safe/tests/test_pam_loginuid.sh
+++ audit/fail-safe/tests/test_pam_loginuid.sh
@@ -18,24 +18,18 @@
 #   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 # =============================================================================
 #
-# FILE		: test_pam_loginuid.sh
-#
-# PURPOSE	: Used to test pam_loginuid and the require_auditd flag
-#
-# DESCRIPTION	: This test runs through the following steps
-#	1. configure pam_loginuid with require_auditd
-#	2. attempt to login with auditd running
-#	3. attempt to login with auditd off
+# PURPOSE:
+# Verify that pam_loginuid.so allows logins when auditd is running, and denies
+# them when it is not.
 # 
-# HISTORY	:
+# HISTORY:
 #  11/05 Initial version by Matt Anderson <mra@hp.com>
 #  11/05 Mods to use global TEST_USER by Aron Griffis <aron@hp.com>
 #
 
 source testcase.bash
 
-export RHOST="localhost"
-# TEST_USER and TEST_USER_PASSWD are set in run.bash startup()
+action=$1
 
 # setup
 # make sure pam_loginuid is configured with require_auditd
@@ -44,11 +38,27 @@ sed -i '/pam_loginuid\.so/s/$/ require_auditd/' /etc/pam.d/sshd || exit_error
 # make sure auditd is running after test
 prepend_cleanup 'killall -0 auditd &>/dev/null || service auditd start'
 
-# attempt to login with auditd running; should work
-./ssh01_s1 || exit_fail
+if [[ $action == "fail" ]]; then
+    service auditd stop || exit_error
+fi
 
-# attempt to login with auditd off; should fail
-service auditd stop || exit_error
-./ssh01_s1 && exit_exit_fail
+# TEST_USER and TEST_USER_PASSWD are exported in run.bash startup()
+expect -c '
+    spawn ssh \
+    -o "PubkeyAuthentication no" \
+    -o "NoHostAuthenticationForLocalhost yes" \
+    -l $env(TEST_USER) localhost whoami
+    expect -nocase {password: $} {
+        send "$env(TEST_USER_PASSWD)\r"
+    }
+    expect "$env(TEST_USER)" {exit 0}
+    exit 1'
 
-exit_pass
+case $?:$action in
+    0:success|1:fail)
+        exit_pass ;;
+    1:success|0:fail)
+        exit_fail ;;
+    *)
+        exit_error ;;
+esac

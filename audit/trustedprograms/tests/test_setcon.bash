0b26c089dcace3677c6e364341358a0204dfa2e1
ljk <ljk@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2007-05-18 23:30:37 +0000

    Add a helper program and two tests for setcon(), which updates /proc/self/attr/current.
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@1803 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

---
+++ audit/trustedprograms/tests/test_setcon.bash
@@ -0,0 +1,102 @@
+#!/bin/bash
+#
+# =============================================================================
+# (c) Copyright Hewlett-Packard Development Company, L.P., 2007
+#
+#   This program is free software;  you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+#   the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program;  if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+# =============================================================================
+#
+# PURPOSE:  This test verifies that writes to /proc/<pid>/attr/current, which
+# is the selinux context of the process, are protected as appropriate.   This
+# test covers the following cases:
+# - success: Where a privileged type (the test harness) is able to update its 
+#   own context
+# - fail_self: Where an unprivileged type (the generic test type) is unable
+#   to update its own context
+#
+# The basic test procedure is:
+# - Attempt to write the 'current' file for the selected test case, using
+#   runcon to run as the appropriate type.
+# - Verify that the "current" context was changed, or not, as expected.
+# - In the success case, verify that the lspp_policy audits the change.
+#
+#   Note:  I'm not checking audit records in the failure case because the
+#   operations fail for different reasons with different syscalls.
+#   Also note, this is in the trusted programs test directory even though
+#   its not testing a trusted program.  Its testing a trusted database
+#   instead but there was no obvious home for the test.
+
+source testcase.bash || exit 2
+
+lspp_policy="lspp_policy"
+current_con=staff_u:lspp_test_r:lspp_harness_t:s0-s15:c0.c1023
+new_pass=staff_u:lspp_test_r:lspp_harness_t:SystemHigh
+new_fail_self=staff_u:lspp_test_r:lspp_test_generic_t:Secret
+auid=$(</proc/self/loginuid)
+
+######################################################################
+# main
+######################################################################
+
+# setup
+op=$1
+
+set -x
+
+# Verify that the lspp_policy module is loaded. We rely on this for
+# the audit records for writing to /proc/self/attr/current.
+semodule -l | grep -q $lspp_policy \
+	|| exit_error "$lspp_policy not loaded"
+
+# Also verify that we're who we think we are
+if [[ $(cat /proc/self/attr/current) != $current_con ]] ; then
+	exit_error "not running as $current_con"
+fi
+
+case $op in 
+	success) 
+		runcon_type=lspp_harness_t
+		new_context=$new_pass
+		;;
+	fail_self)	
+		runcon_type=lspp_test_generic_t
+		new_context=$new_fail_self
+		;;
+	*) 
+		exit_fail "unknown test operation" 
+		;;
+esac
+
+log_mark=$(stat -c %s $audit_log)
+
+# Now update the context using the helper program
+runcon -t $runcon_type -- $TOPDIR/utils/test_setcon $new_context 
+if [[ $? == 0 ]] ; then 
+	# Was it supposed to work?
+	if [[ $op == "success" ]] ; then
+		# Ok, then look for the audit record that the lspp_policy 
+		# causes
+		augrok -q --seek=$log_mark type==SYSCALL syscall=write \
+				subj=$current_con auid=$auid success=yes \
+			|| exit_fail "syscall audit record missing "
+	fi
+	exit_pass
+fi
+# If it failed, was it supposed to?
+if [[ $op == "success" ]] ; then
+	exit_fail "setcon failed"
+fi
+	
+exit_pass
---
+++ audit/utils/test_setcon.c
@@ -0,0 +1,59 @@
+/* =======================================================================
+ *   
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   with this package; if not, write to the Free Software Foundation,
+ *   Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * =======================================================================
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <selinux/selinux.h>
+#include <selinux/context.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	int rc;
+	security_context_t orig_scon;
+	security_context_t new_scon;
+	security_context_t final_scon;
+	
+	if (argc != 2) {
+		fprintf(stderr, "usage: %s new_scon\n", argv[0]);
+		exit(1);
+	}
+
+	rc = getcon(&orig_scon);
+	if (rc < 0) {
+		perror("getcon");
+		exit(1);
+	}
+	new_scon = argv[1];
+	rc = setcon(new_scon);
+	if (rc < 0) {
+		perror("setcon");
+		exit(1);
+	}
+	rc = getcon(&final_scon);
+	if (rc < 0) {
+		perror("getcon");
+		exit(1);
+	}
+	if (strcmp(new_scon, final_scon) != 0) {
+		printf("Failed to change from %s to %s\n",orig_scon,final_scon);
+		exit(1);
+	} 
+	printf("Changed from %s to %s\n",orig_scon,final_scon);
+	exit(0);
+}

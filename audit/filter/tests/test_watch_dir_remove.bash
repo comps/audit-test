15dc3a2a4bf43b5bda6e2afaf8c5792dec68c23d
amg <amg@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2007-07-24 00:05:30 +0000

    Add tests for events in a watched directory.
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@1963 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

---
+++ audit/filter/tests/test_watch_dir_remove.bash
@@ -0,0 +1,57 @@
+#!/bin/bash
+###############################################################################
+# (c) Copyright Hewlett-Packard Development Company, L.P., 2006
+#
+#   This program is free software;  you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+#   the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program;  if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+###############################################################################
+#
+# PURPOSE:
+# Test the ability to filter for objects removed from a watched directory.
+
+source filter_functions.bash || exit 2
+
+# setup
+op=$1
+
+tmpd=$(mktemp -d) || exit_fail "create tempdir failed"
+watch="$tmpd"
+name="$tmpd/foo"
+
+auditctl -a exit,always -S $op -F path=$watch
+
+prepend_cleanup "
+    auditctl -d exit,always -S $op -F path=$watch
+    rm -rf $tmpd"
+
+case $op in
+    rename) touch $name
+            gen_audit_event="mv $tmp1 $name" ;;
+    rmdir)  mkdir $name
+            gen_audit_event="rmdir $name" ;;
+    unlink) touch $name
+            gen_audit_event="rm $name" ;;
+    *) exit_fail "unknown test operation: $op" ;;
+esac
+
+log_mark=$(stat -c %s $audit_log)
+
+# test
+eval "$gen_audit_event"
+
+# verify audit record
+augrok --seek=$log_mark type==SYSCALL syscall==$op name=="$watch/" success==yes \
+    || exit_fail "Expected record not found."
+
+exit_pass

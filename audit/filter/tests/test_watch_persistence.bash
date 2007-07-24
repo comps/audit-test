39629a640dae293448a36d9b7a0bfd56b7417cf6
amg <amg@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2007-07-24 00:26:58 +0000

    Add a test verifying that audit records continue for a watched file when it is removed and created again.
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@1964 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

---
+++ audit/filter/tests/test_watch_persistence.bash
@@ -0,0 +1,60 @@
+#!/bin/bash
+###############################################################################
+# (c) Copyright Hewlett-Packard Development Company, L.P., 2007
+#
+#   This program is free software;  you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+#   the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program;  if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+###############################################################################
+#
+# PURPOSE:
+# Test that audit records continue for a watched file when it is removed and
+# created again.
+
+source filter_functions.bash || exit 2
+
+# setup
+tmpd=$(mktemp -d) || exit_fail "create tempdir failed"
+name="$tmpd/foo"
+
+auditctl -a exit,always -w $name
+
+prepend_cleanup "
+    auditctl -d exit,always -w $name
+    rm -rf $tmpd"
+
+# use syscall utilities
+PATH=$TOPDIR/utils:$TOPDIR/utils/bin:$PATH
+
+# initially create the file; verify audit record
+log_mark=$(stat -c %s $audit_log)
+do_open $name create
+
+augrok --seek=$log_mark type==SYSCALL syscall==open name==$name success==yes \
+    || exit_fail "Expected record not found for initial create of watched file"
+
+# remove the file; verify audit record
+log_mark=$(stat -c %s $audit_log)
+rm $name
+
+augrok --seek=$log_mark type==SYSCALL syscall==unlink name==$name success==yes \
+    || exit_fail "Expected record not found for removal of watched file"
+
+# create the file again; verify audit record
+log_mark=$(stat -c %s $audit_log)
+do_open $name create
+
+augrok --seek=$log_mark type==SYSCALL syscall==open name==$name success==yes \
+    || exit_fail "Expected record not found for re-create of watched file"
+
+exit_pass

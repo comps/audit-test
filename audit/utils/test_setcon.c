112fcaa58c002023e41de0a9a90f36390f7361aa
amg <amg@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2007-08-01 17:41:23 +0000

    Consistently copyright test files, according to the following: - copyright all c sources, scripts, makefiles and policy sources - do not copyright config files or documentation
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@2014 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

--- audit/utils/test_setcon.c
+++ audit/utils/test_setcon.c
@@ -1,5 +1,6 @@
 /* =======================================================================
- *   
+ *   (c) Copyright Hewlett-Packard Development Company, L.P., 2007
+ *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 2 of the License.

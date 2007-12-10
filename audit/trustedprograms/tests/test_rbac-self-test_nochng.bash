08631276fb04344af5d8dcda9c8b3087bdb7cadd
jon <jon@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2007-12-10 19:44:44 +0000

    Increased timeout from 90 to 120
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@2086 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

--- audit/trustedprograms/tests/test_rbac-self-test_nochng.bash
+++ audit/trustedprograms/tests/test_rbac-self-test_nochng.bash
@@ -25,7 +25,7 @@ source testcase.bash || exit 2
 
 # test
 expect -c '
-  set timeout 90
+  set timeout 120 
   spawn newrole -r sysadm_r
   expect -nocase {password: $} {send "$env(PASSWD)\r"}
   send "PS1=\"::\\#$ \"\r";

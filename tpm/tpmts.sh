91598d5ffb996e92716ffb0a0d1fdbab4a81b23b
jon <jon@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2008-04-17 13:41:20 +0000

    add -v to VERSION variable within CheckVars function
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@2093 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

--- tpm/tpmts.sh
+++ tpm/tpmts.sh
@@ -9,13 +9,16 @@ CheckVars ()
 		echo "You must enter a valid path to the testsuite."
 		exit 255
 	fi
+
+	# Add "-v " to VERSION variable.  This is required for the
+	# individual test functions.
+	VERSION="-v $VERSION"
 }
 
 
 InitEnv ()
 {
 	VERSION=${VERSION:="1.2"}
-	VERSION="-v $VERSION"
 
 	# Default independent tests
 	INDTESTS="1 20 6 14 15 16 17 18 21 22 23 24 25 26 27 28 29"

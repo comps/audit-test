d5d796399859a68bb3059625adfc483a9fd31d2f
mra <mra@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2008-06-10 23:57:40 +0000

    Add a check to make sure ownership was taken, and a version variable, also use some additional options on tsstests.sh and pass in any given on the wrapper.sh command line
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@2133 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

--- tss/wrapper.sh
+++ tss/wrapper.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+VERSION=1.2
 TESTSUITE_OWNER_SECRET=abc123
 TESTSUITE_SRK_SECRET=abc123
 export TESTSUITE_OWNER_SECRET TESTSUITE_SRK_SECRET
 
-./tcg/init/Tspi_TPM_TakeOwnership01 -v 1.2
+./tcg/init/Tspi_TPM_TakeOwnership01 -v $VERSION
 
-./tsstests.sh -v 1.2
+if [ $? -ne 0 ]; then
+	echo "Taking ownership failed, aborting test run"
+	exit 1
+fi
+
+./tsstests.sh -v $VERSION -l out-`date +%m%d.%H%M`.log -e err-`date +%m%d.%H%M`.log $@

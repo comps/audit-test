72364de29214287efb9d98d26adb60d720dcb9b2
mra <mra@df539fd5-5ffc-0310-83f5-f045c3a6a7d5>
2008-06-16 15:17:35 +0000

    Add some more checks to taking ownership with the wrapper script
    git-svn-id: http://audit-test.svn.sourceforge.net/svnroot/audit-test/trunk/tests@2135 df539fd5-5ffc-0310-83f5-f045c3a6a7d5

--- tss/wrapper.sh
+++ tss/wrapper.sh
@@ -5,9 +5,23 @@ TESTSUITE_OWNER_SECRET=abc123
 TESTSUITE_SRK_SECRET=abc123
 export TESTSUITE_OWNER_SECRET TESTSUITE_SRK_SECRET
 
-./tcg/init/Tspi_TPM_TakeOwnership01 -v $VERSION
+if [ \! -f tcg/common/common.o ]; then
+	echo "Have you compiled the tests?"
+	exit 1;
+fi
 
-if [ $? -ne 0 ]; then
+if [ \! -x ./tcg/init/Tspi_TPM_TakeOwnership01 ]; then
+	cd tcg/init;
+	make Tspi_TPM_TakeOwnership01
+	if [ $? -ne 0 ]; then
+		echo "Unable to build Tspi_TPM_TakeOwnership01,"\
+			"aborting test run"
+		exit 1
+	fi
+fi
+
+./tcg/init/Tspi_TPM_TakeOwnership01 -v $VERSION
+if [ $? -ne 0 -a $? -ne 8 ]; then
 	echo "Taking ownership failed, aborting test run"
 	exit 1
 fi

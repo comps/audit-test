2e330db9a68d13036480f91075ba7c11f8c5c192
Signed-off-by James Czyzak <czyzak2@linux.vnet.ibm.com>
2011-07-12 17:00:41 -0400

    do_send
    This the function for the utils/bin directory that sets the MSG_OOB bit
    for some data.

---
+++ audit/utils/bin/do_send.c
@@ -0,0 +1,65 @@
+/*  (c) Copyright Hewlett-Packard Development Company, L.P., 2007
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of version 2 the GNU General Public License as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "includes.h"
+
+#define MSG_STRING "Message to run some data over a tcp connection with URG bit set!"
+#define MSG_LEN    (strlen(MSG_STRING) + 1)
+
+int main(int argc, char **argv)
+{
+  int rc, result, sflgs;
+  struct addrinfo *host = NULL;
+  struct addrinfo addr_hints;
+  int sock;
+  int tnum;
+
+  if (argc != 5) {
+    fprintf(stderr, "Usage:\n%s <host> tcp|udp <port> <tnum>\n", argv[0]);
+    return TEST_ERROR;
+  }
+
+  memset(&addr_hints, 0, sizeof(addr_hints));
+  if (strcasecmp(argv[2], "tcp") == 0) {
+    addr_hints.ai_socktype = SOCK_STREAM;
+    addr_hints.ai_protocol = IPPROTO_TCP;
+  } else if (strcasecmp(argv[2], "udp") == 0) {
+    addr_hints.ai_socktype = SOCK_DGRAM;
+    addr_hints.ai_protocol = IPPROTO_UDP;
+  } else
+    return TEST_ERROR;
+  tnum = *argv[4];
+  rc = getaddrinfo(argv[1], argv[3], &addr_hints, &host);
+  if (rc < 0)
+    return TEST_ERROR;
+  sock = socket(host->ai_family, host->ai_socktype, host->ai_protocol);
+  if (sock < 0)
+    return TEST_ERROR;
+
+  errno = 0;
+  rc = connect(sock, host->ai_addr, host->ai_addrlen);
+  if (rc < 0 )
+    return TEST_ERROR;
+
+  errno = 0;
+  sflgs = 0;
+  sflgs |= MSG_OOB;
+  rc = sendto(sock, MSG_STRING, MSG_LEN, sflgs, host->ai_addr, host->ai_addrlen);
+  result = (rc < 0 ? TEST_FAIL : TEST_SUCCESS);
+
+  printf("%d %d %d\n", result, result ? errno : rc, getpid());
+  return result;
+
+}

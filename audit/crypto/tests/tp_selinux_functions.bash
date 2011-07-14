e468cdc925433baf6f3f1ca1d51ce6b606af0e05
Miroslav Vadkerti <mvadkert@redhat.com>
2011-07-14 18:30:26 +0200

    Added selinux helper functions
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

---
+++ audit/crypto/tests/tp_selinux_functions.bash
@@ -0,0 +1,31 @@
+###############################################################################
+#   Copyright (c) 2011 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+###############################################################################
+#
+# Description:
+# Selinux helper functions.
+#
+
+source testcase.bash || exit 2
+
+#### globals
+AUDIT_LOG="/var/log/audit/audit.log"
+
+# Echo block size of audit log (used for augrok)
+function get_audit_mark {
+    echo "$(stat -c %s $AUDIT_LOG)"
+}

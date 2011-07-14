31bf4c921ba50ad0df4418e67942b024fe3d7da9
Miroslav Vadkerti <mvadkert@redhat.com>
2011-07-14 18:30:27 +0200

    Added test ssh_cipher_hmac for FCS_COP.1(NET)
    Detailed description in the test
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

---
+++ audit/crypto/tests/test_ssh_cipher_hmac.bash
@@ -0,0 +1,73 @@
+#!/bin/bash
+###############################################################################
+#   Copyright (c) 2011 Red Hat, Inc. All rights reserved.
+#
+#   This copyrighted material is made available to anyone wishing
+#   to use, modify, copy, or redistribute it subject to the terms
+#   and conditions of the GNU General Public License version 2.
+#
+#   This program is distributed in the hope that it will be
+#   useful, but WITHOUT ANY WARRANTY; without even the implied
+#   warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+#   PURPOSE. See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public
+#   License along with this program; if not, write to the Free
+#   Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#   Boston, MA 02110-1301, USA.
+###############################################################################
+#
+# SFR: FCS_COP.1(NET)
+#
+# AUTHOR: Miroslav Vadkerti <mvadkert@redhat.com>
+#
+# DESCRIPTION:
+# Tries to connect with all required ciphers and HMAC-SHA1 mac:
+# + TDES in CBC mode with 168 bits key size and HMAC-SHA1
+# + AES in CBC mode with 128 bits and 256 bits key size and HMAC-SHA1
+# + AES in CBC mode with 192 bits key size and HMAC-SHA1
+# + AES in CTR mode with 128 bits, 192 bits and 256 bits key size
+#   and HMAC-SHA1
+# Also tests if audit log logs required records with correct ciphers
+#
+
+source testcase.bash || exit 2
+source tp_ssh_functions.bash || exit 2
+source tp_selinux_functions.bash || exit 2
+
+#### main ####
+
+# globals
+PROFILE="/etc/profile"
+
+# be verbose
+set -x
+
+# backup global profile and remove sleep
+backup $PROFILE
+ssh_remove_screen $PROFILE
+
+# enable sysadm_u login via ssh
+setsebool ssh_sysadm_login=1
+
+# Try all key sizes
+for CRYPTO in 3des-cbc aes128-cbc aes192-cbc aes256-cbc aes128-ctr \
+    aes192-ctr aes256-ctr; do
+
+    # test connection from admin->user and vice versa
+    AUDITMARK=$(get_audit_mark)
+    ssh_connect_pass $TEST_ADMIN $TEST_ADMIN_PASSWD \
+        $TEST_USER $TEST_USER_PASSWD "-c $CRYPTO -m hmac-sha1" || \
+        exit_fail "Failed to connect from $TEST_ADMIN to $TEST_USER"
+    ssh_check_audit $AUDITMARK pass $CRYPTO
+    AUDITMARK=$(get_audit_mark)
+    ssh_connect_pass $TEST_USER $TEST_USER_PASSWD \
+        $TEST_ADMIN $TEST_ADMIN_PASSWD "-c $CRYPTO -m hmac-sha1" || \
+        exit_fail "Failed to connect from $TEST_USER to $TEST_ADMIN"
+    ssh_check_audit $AUDITMARK pass $CRYPTO
+done
+
+# disable sysadm_u login via ssh
+setsebool ssh_sysadm_login=0
+
+exit_pass

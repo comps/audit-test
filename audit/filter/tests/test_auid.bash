9c65f49d73a6ed46d6f0c6da728896159ad3cf18
Linda Knippers <linda.knippers@hp.com>
2011-08-28 19:47:31 -0400

    Augment auid test setup
    Many of the filter tests presume that there are no audit
    records for the test operation without the auditctl rule,
    but they don't actually verify that.  Added verification
    to this test case which should be sufficient to demonstrate
    that be default, there are no audit records.
    
    Signed-off-by: Linda Knippers <linda.knippers@hp.com>

--- audit/filter/tests/test_auid.bash
+++ audit/filter/tests/test_auid.bash
@@ -23,6 +23,16 @@ source filter_functions.bash || exit 2
 # setup
 user_auid=$(cat /proc/self/loginuid)
 
+# verify no record by default
+# audit log marker
+log_mark=$(stat -c %s $audit_log)
+
+# generate an audit event
+do_open_file $tmp1
+
+augrok --seek=$log_mark "name==$tmp1" "auid==$user_auid" \
+    && exit_error "Unexpected record found."
+
 auditctl -a exit,always -S open -F auid=$user_auid
 prepend_cleanup "auditctl -d exit,always -S open -F auid=$user_auid"
 

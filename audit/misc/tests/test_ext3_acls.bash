8c7bffd14249e165be1af77ba5efbba89a0668cd
TN, Santhosh <santhosh.tn@hp.com>
2011-10-04 18:39:08 -0500

    Added ext3_acls test
    Signed-off-by: TN, Santhosh <santhosh.tn@hp.com>

---
+++ audit/misc/tests/test_ext3_acls.bash
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+#********************************************************************************
+#	Copyright (C) International Business Machines  Corp., 2000
+#
+#   This program is free software;  you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY;  without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+#   the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program;  if not, write to the Free Software
+#   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
+#  FILE: Ext3_ACLs test
+#
+#  PURPOSE: Wrapper script to run test_ext3_acls.bash script.
+#
+#  HISTORY:
+#               09/11 T.N Santhosh (santhosh.tn@hp.com)
+#
+##################################################################################
+
+source testcase.bash || exit 2
+
+LOG="run.log"
+EXE="ext3acl_file"
+ACL_RUN="./ext3acl_run"
+ACL_TESTS="getfacl-noacl.test setfacl.test ext3acl_permissions.test ext3acl_misc.test"
+
+./$EXE &> $EXE.$LOG
+
+retval+=`grep -v "FAILED" $EXE.$LOG | grep "FAIL" | wc -l`
+echo "TEST PASSED = " `grep "PASS" $EXE.$LOG | wc -l` ", FAILED = " $retval
+
+for file in $ACL_TESTS
+ do
+	if [ ! -e "$file" ]; then
+		echo "$file does not exist."; echo
+		continue;
+	fi
+
+	$ACL_RUN $file &> $file.$LOG;
+	retval2+=`grep -v "FAILED" $file.$LOG | grep "FAIL" | wc -l`
+	echo "TEST PASSED = " `grep "PASS" $file.$LOG | wc -l` ", FAILED = " $retval2
+done
+
+#Checking status of return values
+if [ $(($retval + $retval2)) -gt 0 ]; then
+	exit_fail
+else
+	exit_pass
+fi

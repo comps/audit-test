affc7beb05ed1b3f7b30de3d2345401bb0faf158
Miroslav Vadkerti <mvadkert@redhat.com>
2013-06-18 18:31:41 +0200

    tp/cron_set_valid_mls_level: Improve test coverage
    This patch improves cron_set_valid_mls_level test from trustedprograms.
    It now tests if crond works for SELinux users user_u, staff_u and
    sysadm_u and also checks if the system has configured correct default
    MLS levels for each SELinux user type.
    
    Signed-off-by: Miroslav Vadkerti <mvadkert@redhat.com>

--- audit-test/trustedprograms/tests/test_cron_set_valid_mls_level.bash
+++ audit-test/trustedprograms/tests/test_cron_set_valid_mls_level.bash
@@ -32,8 +32,16 @@
 source testcase.bash || exit 2
 source cron_functions.bash || exit 2
 
-DEF_SEC_LEVEL="SystemLow-SystemHigh"
-TEST_SEC_LEVEL="Secret"
+DEF_SEC_LEVEL_sysadm_u="SystemLow-SystemHigh"
+DEF_SEC_LEVEL_staff_u="SystemLow-SystemHigh"
+DEF_SEC_LEVEL_user_u="SystemLow"
+TEST_SEC_LEVEL_sysadm_u="Secret"
+TEST_SEC_LEVEL_staff_u="Secret"
+# process running as user_u has clearance level only SystemLow at most
+TEST_SEC_LEVEL_user_u="SystemLow"
+
+# check if mcstransd service running
+service mcstransd status || exit_error "mcstransd is not running"
 
 # Prepare environment for test run
 echo "***** Starting cron_set_valid_mls_level test ******"
@@ -44,51 +52,73 @@ test_prep
 # An empty cron.deny file is needed to allow a non-root user to run crontab
 touch $CRON_DENY
 
-semanage login -m -r $DEF_SEC_LEVEL $TEST_USER
+# Try out the cron scenario for user_u, sysadm_u and staff_u users
+for SUSER in staff_u user_u; do
 
-# In order to create a file at a level higher than the lowest level
-# allowed by the range of /tmp, create a directory at the level of which
-# we want to create the file
-chcon -u system_u -t user_tmp_t -l $TEST_SEC_LEVEL $TEST_DIR
+        # Expected default level
+        DEF_SEC_LEVEL=$(eval echo "\$DEF_SEC_LEVEL_$SUSER")
 
-# Add new job
-/bin/su $TEST_USER -c "crontab - << EOF
+        # Clearance level of the crond process running user's job
+        TEST_SEC_LEVEL=$(eval echo "\$TEST_SEC_LEVEL_$SUSER")
+
+        # Set SELinux user with default level
+        semanage login -d $TEST_USER
+        semanage login -a -s $SUSER $TEST_USER
+
+        # Check if default level correct
+        semanage login -l | egrep "${TEST_USER}.*${SUSER}.*$DEF_SEC_LEVEL"
+        if [ $? -ne 0 ]; then
+            semanage login -l
+            cleanup
+            exit_fail "SELinux login record for $TESTUSER is wrong (${TEST_USER}.*${SUSER}.*$DEF_SEC_LEVEL)"
+        fi
+
+        # In order to create a file at a level higher than the lowest level
+        # allowed by the range of /tmp, create a directory at the level of which
+        # we want to create the file
+        chcon -u system_u -t user_tmp_t -l $TEST_SEC_LEVEL $TEST_DIR
+
+        # Add new job
+        /bin/su $TEST_USER -c "crontab - << EOF
 MLS_LEVEL=$TEST_SEC_LEVEL
 * * * * * id >> $TEST_DIR/output_cron 2>&1
 EOF"
 
-# Verify the crontab was successfully added
-if [ $? != 0 ]; then
-        cleanup
-        exit_fail "Error while adding crontab for user $TEST_USER"
-fi
+        # Verify the crontab was successfully added
+        if [ $? != 0 ]; then
+                cleanup
+                exit_fail "Error while adding crontab for user $TEST_USER ($SUSER)"
+        fi
 
-# Change label and owner so crond executes properly
-chcon -t user_cron_spool_t /var/spool/cron/$TEST_USER
-echo "New job added successfully"
+        # Change label and owner so crond executes properly
+        chcon -t user_cron_spool_t /var/spool/cron/$TEST_USER
+        echo "New job added successfully"
 
-# Wait for execution of job
-echo "sleeping for up to 70 seconds..."
-rc=1
-for ((i=0; i<70; i++)); do
-    if test -e $TEST_DIR/output_cron; then
-        rc=0
-        break
-    fi
-    sleep 1
+        # Wait for execution of job
+        echo "sleeping for up to 70 seconds..."
+        rc=1
+        for ((i=0; i<70; i++)); do
+            if test -e $TEST_DIR/output_cron; then
+                rc=0
+                break
+            fi
+            sleep 1
+        done
+
+        if [ $rc = 1 ]; then
+            cleanup
+            exit_fail "Cron did not execute job ($SUSER)"
+        else
+            ls -Z $TEST_DIR/output_cron | grep $TEST_SEC_LEVEL
+            if [ $? = 0 ]; then
+                echo "Job has been executed at the correct MLS_LEVEL specified ($SUSER)"
+                rm -f $TEST_DIR/output_cron
+            else
+                cleanup
+                exit_fail "Job did not execute at the specified MLS_LEVEL ($SUSER)"
+            fi
+        fi
 done
 
-if [ $rc = 1 ]; then
-        cleanup
-        exit_fail "Cron did not execute job"
-else
-        ls -Z $TEST_DIR/output_cron | grep $TEST_SEC_LEVEL
-        if [ $? = 0 ]; then
-                echo "Job has been executed at the correct MLS_LEVEL specified"
-                cleanup
-                exit_pass "cron_set_valid_mls_level"
-        else
-                cleanup
-                exit_fail "Job did not execute at the specified MLS_LEVEL"
-        fi
-fi
+cleanup
+exit_pass "cron_set_valid_mls_level"

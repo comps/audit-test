f1c25974c41b82ae659a80946dda7842a59af870
Jiri Jaburek <jjaburek@redhat.com>
2014-05-15 16:46:07 +0200

    utils/bin: added do_setns
    Signed-off-by: Jiri Jaburek <jjaburek@redhat.com>

---
+++ audit-test/utils/bin/do_setns.c
@@ -0,0 +1,44 @@
+/* Copyright (c) 2014 Red Hat, Inc. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 2 the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "includes.h"
+#include <sched.h>
+
+int main(int argc, char **argv)
+{
+    int fd, rc, result;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage:\n%s <nshandle>\n", argv[0]);
+        return TEST_ERROR;
+    }
+
+    fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        return TEST_ERROR;
+    }
+
+    /* namespace type checking not implemented, use 0 */
+    rc = setns(fd, 0);
+    result = (rc == -1) ? TEST_FAIL : TEST_SUCCESS;
+
+    close(fd);
+
+    fprintf(stderr, "%d %d %d\n", result, result ? errno : rc, getpid());
+    return result;
+}
+
+/* vim: set sts=4 sw=4 et : */

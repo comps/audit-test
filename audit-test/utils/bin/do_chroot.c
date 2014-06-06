cf7569ee1dff71f8b5daf299426576a02a78a670
Matus Marhefka <mmarhefk@redhat.com>
2014-06-06 09:11:43 +0200

    utils/bin: added do_chroot
    Signed-off-by: Matus Marhefka <mmarhefk@redhat.com>

---
+++ audit-test/utils/bin/do_chroot.c
@@ -0,0 +1,33 @@
+/* Copyright (c) 2014 Red Hat, Inc. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 2 the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "includes.h"
+
+int main(int argc, char **argv)
+{
+    int exitval, result;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage:\n%s <path>\n", argv[0]);
+        return TEST_ERROR;
+    }
+
+    errno = 0;
+    exitval = chroot(argv[1]);
+    result = exitval < 0;
+
+    fprintf(stderr, "%d %d %d\n", result, result ? errno : exitval, getpid());
+    return result;
+}

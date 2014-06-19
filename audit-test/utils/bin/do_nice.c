4e5d8e75540be60bcf1dcc2957398dc3fdacf600
Matus Marhefka <mmarhefk@redhat.com>
2014-06-19 13:28:54 +0200

    utils/bin: added do_nice
    Signed-off-by: Matus Marhefka <mmarhefk@redhat.com>

---
+++ audit-test/utils/bin/do_nice.c
@@ -0,0 +1,35 @@
+/* Copyright (c) 2014 Red Hat, Inc. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 2 the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "includes.h"
+
+int main(int argc, char **argv)
+{
+    int exitval, result;
+    int inc;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage:\n%s <inc>\n", argv[0]);
+        return TEST_ERROR;
+    }
+    inc = strtol(argv[1], NULL, 10);
+
+    errno = 0;
+    exitval = syscall(__NR_nice, inc);
+    result = exitval < 0;
+
+    fprintf(stderr, "%d %d %d\n", result, result ? errno : exitval, getpid());
+    return result;
+}

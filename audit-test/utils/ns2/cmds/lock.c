9ef0b84758e3e8b673b876c2761298fbaa39f8e3
Jiri Jaburek <jjaburek@redhat.com>
2015-08-06 14:36:13 +0200

    utils/ns2: avoid writer starvation on lock
    Besides the actual gate+lock implementation, we also make lock type
    specification mandatory and drop the upgrade/downgrade concept.
    It sounded good in theory, but real use cases tend to know the wanted
    lock type from the start.
    
    Signed-off-by: Jiri Jaburek <jjaburek@redhat.com>

--- audit-test/utils/ns2/cmds/lock.c
+++ audit-test/utils/ns2/cmds/lock.c
@@ -6,78 +6,104 @@
 #include <sys/file.h>
 #include "shared.h"
 
-#define GLOBAL_LOCK_FILE "./ns2.lock"
+#define LOCK_FILE "./ns2.lock"
+#define LOCK_GATE_FILE "./ns2-gate.lock"
 
-/* gain/release or up/downgrade a lock over the entire server
+/* gain/release a lock over the entire server
  *
- * - with explicit action:
- *   - `lock,sh' / `lock,ex' takes a shared/exclusive lock
- *   - `unlock,all' unlocks any lock
+ * since the default flock/ioctl lock implementation is reader-biased,
+ * making writers starve on a busy server, we use an extra mutex called
+ * "gate" here - the idea being that both readers and writers have an
+ * equal chance of locking the gate, followed by:
+ *   - readers: shared-locking the actual lock, unlocking the gate
+ *   - writers: exclusive-locking the actual lock, keeping gate locked,
+ *              preventing more readers from getting through
  *
- * - without specified action:
- *   - `lock' upgrades a lock (no lock -> shared -> exclusive)
- *   - `unlock' downgrades a lock (exclusive -> shared -> no lock)
+ * the result is a write-biased lock, but without an explicit reader
+ * or writer starvation (as both have an equal chance at the gate)
  */
 
 /* args:
- *   lock,[sh|ex],[nonblock]
- *   unlock,[all]
+ *   lock,<sh|ex>,[nonblock]
+ *   unlock
  */
 
 static int lockfd = -1;
-static int lock_held = 0;
+static int gatefd = -1;
+
+/* note: when doing exclusive->shared, this algorithm could deadlock
+ * when acquiring gate on some locking backends, but flock(2) lets us
+ * call LOCK_EX multiple times on the same descriptor */
+int xflock(int gate, int lock, int op)
+{
+    int nb = (op & LOCK_NB);
+
+    if (op & LOCK_UN) {
+        if (flock(lock, LOCK_UN) == -1)
+            return -1;
+        if (flock(gate, LOCK_UN) == -1)
+            return -1;
+    }
+    if (op & (LOCK_EX|LOCK_SH)) {
+        if (flock(gate, LOCK_EX | nb) == -1)
+            return -1;
+        if (flock(lock, op) == -1) {
+            flock(gate, LOCK_UN);
+            return -1;
+        }
+    }
+    if (op & LOCK_SH) {
+        if (flock(gate, LOCK_UN) == -1) {
+            flock(lock, LOCK_UN);
+            return -1;
+        }
+    }
+    return 0;
+}
 
 static int cmd_lock(int argc, char **argv, struct session_info *info)
 {
     UNUSED(info);
-    int type = 0;
+    int type;
 
-    if (argc > 1 && !strcmp(argv[1], "sh")) {
+    if (argc < 2)
+        return -1;  /* dangerous to go on without a lock */
+
+    if (!strcmp(argv[1], "sh")) {
         type = LOCK_SH;
-        argv++;
-        argc--;
-    } else if (argc > 1 && !strcmp(argv[1], "ex")) {
+    } else if (!strcmp(argv[1], "ex")) {
         type = LOCK_EX;
-        argv++;
-        argc--;
+    } else {
+        return -1;
     }
 
-    if (argc > 1 && !strcmp(argv[1], "nonblock")) {
+    if (argc >= 3 && !strcmp(argv[2], "nonblock")) {
         type |= LOCK_NB;
     }
 
     if (lockfd == -1) {
-        lockfd = open(GLOBAL_LOCK_FILE,
-                      O_RDONLY|O_CREAT|O_NOCTTY|O_CLOEXEC, 0600);
+        lockfd = open(LOCK_FILE, O_RDONLY|O_CREAT|O_NOCTTY|O_CLOEXEC, 0600);
         if (lockfd == -1) {
             perror("lock/open");
-            return -1;  /* dangerous to go on without a lock */
+            return -1;
+        }
+    }
+    if (gatefd == -1) {
+        gatefd = open(LOCK_GATE_FILE, O_RDONLY|O_CREAT|O_NOCTTY|O_CLOEXEC, 0600);
+        if (gatefd == -1) {
+            perror("lock/open");
+            return -1;
         }
     }
 
-    /* default argument-less action:
-     * - if no lock is held, get a shared one
-     * - if a shared lock is held, upgrade to exclusive
-     */
-    if (!(type & (LOCK_SH | LOCK_EX))) {
-        if (!lock_held) {
-            type |= LOCK_SH;
-        } else if (lock_held & LOCK_SH) {
-            type |= LOCK_EX;
-        } else if (lock_held & LOCK_EX) {
-            type |= LOCK_EX;
-        }
-    }
-
-    if (flock(lockfd, type) == -1) {
+    if (xflock(gatefd, lockfd, type) == -1) {
         if (errno == EWOULDBLOCK) {
             return 2;  /* lock is already held */
         } else {
             perror("lock");
-            return -1;  /* dangerous to go on without a lock */
+            return -1;
         }
     }
-    lock_held = type & (LOCK_SH | LOCK_EX);
 
     return 0;
 }
@@ -85,34 +111,19 @@ static int cmd_lock(int argc, char **argv, struct session_info *info)
 static int cmd_unlock(int argc, char **argv, struct session_info *info)
 {
     UNUSED3(argc, argv, info);
-    int op;
 
-    if (argc >= 2 && !strcmp(argv[1], "all")) {
-        op = LOCK_UN;
-    } else {
-        /* just downgrade the lock:
-         * - if an exclusive lock is held, downgrade it to shared
-         * - if a shared lock is held, unlock
-         */
-        if (lock_held & LOCK_EX)
-            op = LOCK_SH;
-        else
-            op = LOCK_UN;
-    }
-
-    if (flock(lockfd, op) == -1) {
+    if (xflock(gatefd, lockfd, LOCK_UN) == -1) {
         perror("unlock");
         return -1;  /* continuing could cause deadlock! */
     }
-
-    lock_held = op & LOCK_SH;
 
     return 0;
 }
 
 static void cmd_lock_cleanup(void)
 {
-    unlink(GLOBAL_LOCK_FILE);
+    unlink(LOCK_FILE);
+    unlink(LOCK_GATE_FILE);
 }
 
 static __newcmd struct cmd_desc cmd1 = {
